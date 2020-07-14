d2a215b3c199974a62d1929549413be408c726b1
nicol-hawkins <nicolchawkins@gmail.com>
2020-07-13 20:46:26 -0700

    removed parenthesis after componentDidMount. fetch failed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  componentDidMount() {
+    fetch("https://ronreiter-meme-generator.p.rapidapi.com/meme?font=Impact&font_size=50&meme=Condescending-Wonka&top=Top%20text&bottom=Bottom%20text", {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-host": "ronreiter-meme-generator.p.rapidapi.com",
+        "x-rapidapi-key": "SIGN-UP-FOR-KEY"
+      }
+  })
+  .then(response => {
+	  console.log(response);
+  })
+  .catch(err => {
+	  console.log(err);
+  });
+  }
+
+  
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Meme Generator</h1>
+          <div className="memeButtonDiv">
+            <button className="memeButton">
+              Click Me
+            </button>
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
