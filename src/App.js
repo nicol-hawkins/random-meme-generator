10fd18730bd66249b63860770389c08036c58864
nicol-hawkins <nicolchawkins@gmail.com>
2020-07-13 22:07:55 -0700

    error 500, cant make fetch request to api yet

--- src/App.js
+++ src/App.js
@@ -3,22 +3,26 @@ import './App.css';
 
 class App extends Component {
 
+  
+
   componentDidMount() {
-    fetch("https://ronreiter-meme-generator.p.rapidapi.com/meme?font=Impact&font_size=50&meme=Condescending-Wonka&top=Top%20text&bottom=Bottom%20text", {
-      "method": "GET",
-      "headers": {
-        "x-rapidapi-host": "ronreiter-meme-generator.p.rapidapi.com",
-        "x-rapidapi-key": "SIGN-UP-FOR-KEY"
-      }
-  })
-  .then(response => {
-	  console.log(response);
-  })
-  .catch(err => {
-	  console.log(err);
-  });
+    fetch("https://cors-anywhere.herokuapp.com/https://ronreiter-meme-generator.p.rapidapi.com/meme?font=Impact&font_size=50&meme=Condescending-Wonka&top=Top%20text&bottom=Bottom%20text",
+      {
+        "method": "GET",
+        "headers": {
+          "x-rapidapi-host": "ronreiter-meme-generator.p.rapidapi.com",
+          "x-rapidapi-key": "SIGN-UP-FOR-KEY"
+        }
+      })
+      .then(response => {
+        console.log(response);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
+  
   
 
   render() {
